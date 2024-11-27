36968e13f513c3bff576eb213cb7f341ab359a4c
Suprith22 <amathisuprith@gmail.com>
2024-11-27 15:52:11 +0530

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+float avg(int ar[], int size) 
+{ 
+    int s = 0;
+    for (int i = 0; i < size; i++)
+	{
+        s += ar[i];
+    }
+    return float(s / size);
+}
+int main() 
+{
+    int ar[5] = {10, 20, 30, 40, 50};
+    printf("Average: %f", avg(ar, 5));
+    return 0;
+}  
